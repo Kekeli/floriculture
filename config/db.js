5e22d548e4c126007223aa43041d136bc5248d48
Kelley Scott <kekeli.scott@gmail.com>
2014-10-27 08:34:24 -0400

    Add missing db.js changes

--- config/db.js
+++ config/db.js
@@ -3,16 +3,19 @@ var mongoose = require( 'mongoose');
 // connect to database
 module.exports = {
 
+  url: 'mongodb://localhost/greenhouse',
+ 
   // initialize DB
   startup: function() {
-    // now connect to the gardens db
+
+    // now connect to the greenhouse db
     mongoose.connect( 'mongodb://localhost/greenhouse' );
 
     var db = mongoose.connection;
     // Check connection to mongoDB
     db.on('error', console.error.bind(console, 'DB connection error:'));
     db.once('open', function() {
-      console.log('We have connected to the mongodb for the greenhouse');
+      console.log('We have connected to mongodb for the greenhouse');
     }); 
   },
 
